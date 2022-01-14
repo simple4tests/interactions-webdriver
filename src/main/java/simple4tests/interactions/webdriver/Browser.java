77d0e375e37a75ab87aab4aaf66d3c6b7cf24739
frederic-gs <frederic.goncalves@vd.ch>
2022-01-14 09:04:26 +0100

    clean imports

--- src/main/java/simple4tests/interactions/webdriver/Browser.java
+++ src/main/java/simple4tests/interactions/webdriver/Browser.java
@@ -25,7 +25,6 @@ SOFTWARE.
 package simple4tests.interactions.webdriver;
 
 import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
